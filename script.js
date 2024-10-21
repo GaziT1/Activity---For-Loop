c38da8f85ce201bed5ec73c423ffa2d8fcfac9de
GaziT1 <147172459+GaziT1@users.noreply.github.com>
2024-10-21 10:23:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
 window.addEventListener("load",addListener);
-var count
+var count = "";
 function addListener()
 {
-	document.getElementById("btnsubmit").addEventListener("click",Howmnygrdes);
+		document.getElementById("btnsubmit").addEventListener("click",Check);
 }
-
+function Check()
+{
+	if (count == "")
+		{
+			Howmnygrdes()
+		}
+	else
+	{
+		Grades(count)
+	}
+}
 function Howmnygrdes()
 {
 	count = document.getElementById("txtinput").value;
-	if (count % 2 == 0)
+	if (count % 1 == 0)
 		{
 			document.getElementById("lblinput").textContent = "What are the grades? ";
 			document.getElementById("txtinput").value = ""
-			Grades(count)
 		}
 	else
 	{
 		alert("Whole Numbers Please!")
 		document.getElementById("txtinput").value = ""
-		Howmnygrdes()
 	}
 
 }
 
 function Grades(times)
 {
-	
+	var gradeinput = 0
+	var grade = 0;
+	for (let i = 0; i <= parseInt(times); i++)
+		{
+			gradeinput = parseFloat(document.getElementById("txtinput").value)
+			if (gradeinput <= 100.00 && gradeinput >= 0.00)
+				{
+					grade = grade + parseFloat(document.getElementById("txtinput").value)
+				}
+			else 
+			{
+				alert("Number must be within 0 and 100!")
+				document.getElementById("txtinput").value = ""
+			}
+		}
+	document.getElementById("lbloutput").textContent = "Grade average: " + grade
 }
