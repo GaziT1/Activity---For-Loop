2a6ef7d8ec06696621d18847bf3868e85113a578
GaziT1 <147172459+GaziT1@users.noreply.github.com>
2024-10-22 10:23:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.addEventListener("load",addListener);
-var count = "", grade = 0, gradeinput = 0;
+var count = "", gradeinput = 0, sum = 0, count2 = 0;
 function addListener()
 {
 		document.getElementById("btnsubmit").addEventListener("click",Check);
@@ -18,14 +18,14 @@ function Check()
 function Howmnygrdes()
 {
 	count = document.getElementById("txtinput").value;
-	if (count % 1 == 0)
+	if (count % 1 !== 0 || count == "" || count < 1)
 		{
-			document.getElementById("lblinput").textContent = "What are the grades? ";
+			alert("Whole Numbers Please!")
 			document.getElementById("txtinput").value = ""
 		}
 	else
 	{
-		alert("Whole Numbers Please!")
+		document.getElementById("lblinput").textContent = "What are the grades? ";
 		document.getElementById("txtinput").value = ""
 	}
 
@@ -33,21 +33,30 @@ function Howmnygrdes()
 
 function Grades(times)
 {
-	for (let i = 0; i < parseInt(times); i++)
-	{
+	
 		gradeinput = parseFloat(document.getElementById("txtinput").value)
-		if (gradeinput > 100.00 || gradeinput < 0.00)
+		if (gradeinput > 100.00 || gradeinput < 0.00 || gradeinput == "")
 			{
 				alert("Number must be within 0 and 100!")
 				document.getElementById("txtinput").value = ""
 			}
 		else 
 			{
-				grade = grade + parseFloat(document.getElementById("txtinput").value)
+				for (let i = 0; i < parseInt(times); i++)
+					{
+						sum = sum + gradeinput
+						gradeinput = 0;
+						var gradeaverage = sum/times
+						var gradecollegescale = gradeaverage/25
+						document.getElementById("txtinput").value = ""
+						document.getElementById("txtinput").focus
+					}
+				count2 += 1
+				if (count2 == count)
+					{
+						document.getElementById("txtinput").disabled = true;
+						document.getElementById("lbloutput").textContent = "Grade average: " + gradeaverage + "  College Scale: " + gradecollegescale
+					}
+										
 			}
-	}
-	var gradeaverage = grade/times
-	var gradecollegescale = gradeaverage/25
-	gradeinput = 0;
-	document.getElementById("lbloutput").textContent = "Grade average: " + gradeaverage + "  College Scale: " + gradecollegescale
 }
